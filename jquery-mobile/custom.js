f17480c7d342674ab45cf70a45ead0e8c8783f87
Betsy Muriithi <bnmuriithi@gmail.com>
2013-02-13 11:10:16 +0300

    Changes to UI look and feel
    Changed colors to look like the psd files..
    added jquery code to connect to the sqllite database.

--- jquery-mobile/custom.js
+++ jquery-mobile/custom.js
@@ -1,17 +1,21 @@
 // JavaScript Document
 //connect to database
-//var username = "user2", pwd = "user2";
+var username = "user3";
+var pwd = "user3";
 
 $(document).bind("mobileinit", function(){
 	//overide to open db
-	//$.mobile.db = openDatabase('lawone', '1.0', 'lawone database', 2*1024*1024);
-	//$.mobile.db.transaction(function(t) {
-			//t.executeSql('CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, pwd TEXT)');
-			//t.executeSql('INSERT INTO users(username, pwd) VALUES (?,?)',[username, pwd]);
-    //});
+	$.mobile.db = openDatabase('lawone', '1.0', 'lawone database', 2*1024*1024);
+	$.mobile.db.transaction(function(t) {
+		//t.executeSql('DROP TABLE IF EXISTS users');
+	//t.executeSql("CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, pwd TEXT)");
+	t.executeSql("CREATE TABLE IF NOT EXISTS file (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,fileNumber VARCHAR NOT NULL, lastUpdate VARCHAR, partner TEXT)")
+	t.executeSql("INSERT INTO file(fileNumber, lastUpdate, partner) VALUES (?,?,?)",["123/ref/2012","5/11/2012","Melvin Kamau"]);
+	
+    });
 	
 	//menu show and hide
-$('#myPage').live('pageinit', function(event) {
+$('.myPage').live('pageinit', function(event) {
 	$("#hide").hide();
     $("#show_menu").bind('tap',function(event, ui){
         $('#menu').show('fast', function() {});
@@ -55,9 +59,24 @@ $('#myPage').live('pageinit', function(event) {
 		
 		});
 	});
+	
 		
 });
 //end show and hide
+
+	//function showAll(file_id ,file_num,file_date){
+//		$("#file").apppend(
+//		
+//		'<li data-theme="c" class="files">' +
+//        '<a href="#popupPanel" data-rel="popup" data-transition="slide" data-position-to="window" ' +
+//	    'data-user="'+ row.id + '"><img src="icons/star.jpg" alt="" class="ui-li-icon ui-li-thumb"> ' +
+//        ' <hgroup><h1>' + row.filenumber + '</h1><h2>' + row.lastUpdate + ' | ' + row.partner + ' </h2></hgroup> '+
+//        '    </a> ' +
+//        '    <span id="notification" class="ui-btn-up-b ui-btn-corner-all">Notifications 4</span> '+
+//        '</li>'
+//		
+//		);
+//	}
 });
 
 
@@ -79,18 +98,16 @@ $("div[data-role=page]").bind("pagebeforeshow", function (e, data) {
     $.mobile.changePage.defaults.transition = 'slide';
 });
 
-
-$(function() {$('#dashboard').live("pagebeforeshow",getUsers);
-		//$('#new').live('pageshow', getLocation);
-		//$('#insert').live('submit', insertEntry);
+$('#page_files').live('pageinit', function(event){
+	$('#content').append(getFiles)
 });
 
-function getUsers() {
-	var list = $('#list'), items = [];
+function getFiles() {
+	var list = $('#file'), items = [];
 	
 	$.mobile.db.transaction(function(t) {
 		//get records
-		t.executeSql('SELECT id, username,pwd FROM users ORDER BY id ',[],
+		t.executeSql("SELECT id, fileNumber, lastUpdate, partner FROM file ORDER BY lastUpdate ",[],
 		
 		//display records
 		function(t, result) {
@@ -101,17 +118,27 @@ function getUsers() {
 			if (len > 0 ) {
 				
 				for (i = 0; i < len; i += 1) {
+					
 					row = result.rows.item(i);
-					items.push(//'<li><a href="#display" data-user="'+ row.id + '">' + row.username + '</a></li>'
-					'<tr><td><a href="#display" data-user="'+ row.id + '">' + row.username + '</a></td><td>' + row.pwd + '</td></tr>');
-					}
+					
+					items.push(
+				   '<li data-theme="c" class="files">' +
+                   '<a href="#popupPanel" data-rel="popup" data-transition="slide" data-position-to="window" ' +
+				   'data-user="'+ row.id + '"><img src="icons/star.jpg" alt="" class="ui-li-icon ui-li-thumb"> ' +
+                   ' <hgroup><h1>' + row.fileNumber + '</h1><h2>' + row.lastUpdate + ' | ' + row.partner + ' </h2></hgroup> '+
+                    '    </a> ' +
+                    '    <span id="notification" class="ui-btn-up-b ui-btn-corner-all">Notifications 4</span> '+
+                   '</li>'
+					
+					);
+				}
 					
 				list.html(items.join());
-				//list.listview('refresh');
+				list.listview('refresh');
 				
-				$('a', list).live('click',function(e) {
+				/*$('a', list).live('click',function(e) {
 					getItem(($(this).attr('data-user')));
-					});
+					});*/
 			} 
 		});
 		
